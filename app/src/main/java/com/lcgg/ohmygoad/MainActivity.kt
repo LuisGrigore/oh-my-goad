7101679be84d6db4c46d8820d840f6379f9e7b67
Luis Grigore <64846391+LuisGrigore@users.noreply.github.com>
2026-09-04 20:59:02 +0200

    Waa

--- app/src/main/java/com/lcgg/ohmygoad/MainActivity.kt
+++ app/src/main/java/com/lcgg/ohmygoad/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lcgg.ohmygoad.ui.theme.OhMyGOADTheme
-
+//WAAA
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
