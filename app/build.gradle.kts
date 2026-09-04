7bc5a78998d296c9ce2086928dac27324f62fc18
Luis Grigore <64846391+LuisGrigore@users.noreply.github.com>
2026-09-04 15:44:44 +0200

    Created pr-checks.yml workflow and configured gradle for versionning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,31 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
 }
+val versionPropsFile = rootProject.file("version.properties")
+val versionProps = Properties()
+if (versionPropsFile.exists()) {
+    versionProps.load(FileInputStream(versionPropsFile))
+}
+val appVersionCode = versionProps.getProperty("versionCode", "1").toInt()
+val appVersionName = versionProps.getProperty("versionName", "1.0")
 
 android {
     namespace = "com.lcgg.ohmygoad"
     compileSdk {
         version = release(37)
     }
-
     defaultConfig {
         applicationId = "com.lcgg.ohmygoad"
         minSdk = 33
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = appVersionCode
+        versionName = appVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             optimization {
